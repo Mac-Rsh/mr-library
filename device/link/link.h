4aa51ea776bdd98e84474cded4bea936157fcc84
MacRsh <1063220965@qq.com>
2023-03-15 01:40:05 +0800

    暂存link思路

--- device/link/link.h
+++ device/link/link.h
@@ -17,46 +17,46 @@
 
 enum mr_link_state
 {
-    MR_LINK_CHECK_HEAD,
-    MR_LINK_CHECK_DATA,
-    MR_LINK_OK,
+	MR_LINK_CHECK_HEAD,
+	MR_LINK_CHECK_DATA,
+	MR_LINK_OK,
 };
 
 struct mr_link_packet_head
 {
-    mr_uint8_t frame_head: 7;
-    mr_uint8_t ack: 1;
-    mr_uint8_t src_addr;
-    mr_uint8_t dst_addr;
-    mr_uint8_t sequence;
-    mr_uint16_t length;
-    mr_uint32_t crc;
+	mr_uint8_t frame_head: 7;
+	mr_uint8_t ack: 1;
+	mr_uint8_t src_addr;
+	mr_uint8_t dst_addr;
+	mr_uint8_t sequence;
+	mr_uint16_t length;
+	mr_uint32_t crc;
 };
 
 typedef struct mr_link *mr_link_t;
 
 struct mr_link_service
 {
-    struct mr_device device;
+	struct mr_device device;
 
-    struct mr_list list;
-    mr_uint8_t src_addr;
+	struct mr_list list;
+	mr_uint8_t src_addr;
 };
 typedef struct mr_link_service *mr_link_service_t;
 
 struct mr_link
 {
-    struct mr_device device;
+	struct mr_device device;
 
-    struct mr_list list;
-    mr_uint8_t buffer[MR_LINK_PACKET_MAX];
+	struct mr_list list;
+	mr_uint8_t buffer[MR_LINK_PACKET_MAX];
 };
 
 mr_size_t mr_link_write(mr_link_t link,
-                        mr_uint8_t src_addr,
-                        mr_uint8_t dst_addr,
-                        mr_uint8_t *buffer,
-                        mr_size_t count);
+						mr_uint8_t src_addr,
+						mr_uint8_t dst_addr,
+						mr_uint8_t *buffer,
+						mr_size_t count);
 
 mr_err_t mr_link_service_add_to_container(mr_link_service_t link_service, const char *name);
 mr_err_t mr_link_add_to_container(mr_link_t link, const char *name, char *port_name);
