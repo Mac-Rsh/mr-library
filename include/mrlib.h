8579b5b857b208f8848ad1f38f654cf97ac27b2e
MacRsh <1063220965@qq.com>
2023-03-16 11:13:38 +0800

    更新ringbuffer API

--- include/mrlib.h
+++ include/mrlib.h
@@ -27,14 +27,12 @@ void mr_mutex_init(mr_mutex_t mutex);
 mr_err_t mr_mutex_take(mr_mutex_t mutex, mr_object_t owner);
 mr_err_t mr_mutex_release(mr_mutex_t mutex, mr_object_t owner);
 
-void mr_ringbuffer_init(mr_ringbuffer_t ringbuffer, mr_uint8_t *pool, mr_size_t pool_size);
-mr_size_t mr_ringbuffer_get_data_length(mr_ringbuffer_t ringbuffer);
-mr_size_t mr_ringbuffer_write_byte(mr_ringbuffer_t ringbuffer, mr_uint8_t data);
-mr_size_t mr_ringbuffer_write(mr_ringbuffer_t ringbuffer, const mr_uint8_t *buffer, mr_size_t length);
-mr_size_t mr_ringbuffer_write_data_force(mr_ringbuffer_t ringbuffer, mr_uint8_t data);
-mr_size_t mr_ringbuffer_write_force(mr_ringbuffer_t ringbuffer, const mr_uint8_t *buffer, mr_size_t length);
-mr_size_t mr_ringbuffer_read(mr_ringbuffer_t ringbuffer, mr_uint8_t *buffer, mr_size_t length);
-mr_size_t mr_ringbuffer_read_data(mr_ringbuffer_t ringbuffer, mr_uint8_t *data);
+void mr_ringbuffer_init(mr_ringbuffer_t rb, mr_uint8_t *pool, mr_size_t pool_size);
+void mr_ringbuffer_reset(mr_ringbuffer_t rb);
+mr_size_t mr_ringbuffer_get_data_length(mr_ringbuffer_t rb);
+mr_size_t mr_ringbuffer_read(mr_ringbuffer_t ringbuffer, mr_uint8_t *buffer, mr_size_t count);
+mr_size_t mr_ringbuffer_write(mr_ringbuffer_t ringbuffer, const mr_uint8_t *buffer, mr_size_t count);
+mr_size_t mr_ringbuffer_write_force(mr_ringbuffer_t rb, const mr_uint8_t *buffer, mr_size_t count);
 
 mr_device_t mr_device_find(const char *name);
 mr_err_t mr_device_add_to_container(mr_device_t device,
@@ -46,7 +44,7 @@ mr_err_t mr_device_add_to_container(mr_device_t device,
 mr_err_t mr_device_open(mr_device_t device, mr_uint16_t flags);
 mr_err_t mr_device_close(mr_device_t device);
 mr_err_t mr_device_ioctl(mr_device_t device, int cmd, void *args);
-mr_size_t mr_device_read(mr_device_t device, mr_off_t pos, void *buffer, mr_size_t size);
-mr_size_t mr_device_write(mr_device_t device, mr_off_t pos, const void *buffer, mr_size_t size);
+mr_size_t mr_device_read(mr_device_t device, mr_off_t pos, void *buffer, mr_size_t count);
+mr_size_t mr_device_write(mr_device_t device, mr_off_t pos, const void *buffer, mr_size_t count);
 
 #endif
