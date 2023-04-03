2395a1b3f1abd382c80c532b2a7ea0b79875862a
MacRsh <1063220965@qq.com>
2023-04-03 11:57:37 +0800

    修复: 1.开关中断锁顺序错误。

--- src/ringbuffer.c
+++ src/ringbuffer.c
@@ -28,7 +28,7 @@ void mr_ringbuffer_reset(mr_ringbuffer_t rb)
 {
 	MR_ASSERT(rb != MR_NULL);
 
-	mr_hw_interrupt_enable();
+	mr_hw_interrupt_disable();
 
 	rb->read_index = 0;
 	rb->write_index = 0;
@@ -36,28 +36,38 @@ void mr_ringbuffer_reset(mr_ringbuffer_t rb)
 	rb->read_mirror = 0;
 	rb->write_mirror = 0;
 
-	mr_hw_interrupt_disable();
+	mr_hw_interrupt_enable();
 }
 
 mr_size_t mr_ringbuffer_get_data_length(mr_ringbuffer_t rb)
 {
-	mr_hw_interrupt_enable();
+	mr_hw_interrupt_disable();
 
 	/* Empty or full according to the mirror flag */
 	if (rb->read_index == rb->write_index)
 	{
 		if (rb->read_mirror == rb->write_mirror)
+		{
+			mr_hw_interrupt_enable();
 			return 0;
+		}
 		else
+		{
+			mr_hw_interrupt_enable();
 			return rb->size;
+		}
 	}
 
 	if (rb->write_index > rb->read_index)
+	{
+		mr_hw_interrupt_enable();
 		return rb->write_index - rb->read_index;
+	}
 	else
+	{
+		mr_hw_interrupt_enable();
 		return rb->size - rb->read_index + rb->write_index;
-
-	mr_hw_interrupt_disable();
+	}
 }
 
 mr_size_t mr_ringbuffer_read(mr_ringbuffer_t rb, mr_uint8_t *buffer, mr_size_t count)
@@ -81,7 +91,7 @@ mr_size_t mr_ringbuffer_read(mr_ringbuffer_t rb, mr_uint8_t *buffer, mr_size_t c
 	if (count > length)
 		count = length;
 
-	mr_hw_interrupt_enable();
+	mr_hw_interrupt_disable();
 
 	/* Copy the data from the ringbuffer to the buffer */
 	if ((rb->size - rb->read_index) > count)
@@ -89,8 +99,7 @@ mr_size_t mr_ringbuffer_read(mr_ringbuffer_t rb, mr_uint8_t *buffer, mr_size_t c
 		mr_memcpy(buffer, &rb->buffer[rb->read_index], count);
 		rb->read_index += count;
 
-		mr_hw_interrupt_disable();
-
+		mr_hw_interrupt_enable();
 		return count;
 	}
 
@@ -100,8 +109,7 @@ mr_size_t mr_ringbuffer_read(mr_ringbuffer_t rb, mr_uint8_t *buffer, mr_size_t c
 	rb->read_mirror = ~ rb->read_mirror;
 	rb->read_index = count - (rb->size - rb->read_index);
 
-	mr_hw_interrupt_disable();
-
+	mr_hw_interrupt_enable();
 	return count;
 }
 
@@ -126,7 +134,7 @@ mr_size_t mr_ringbuffer_write(mr_ringbuffer_t rb, const mr_uint8_t *buffer, mr_s
 	if (count > length)
 		count = length;
 
-	mr_hw_interrupt_enable();
+	mr_hw_interrupt_disable();
 
 	/* Copy the data from the buffer to the ringbuffer */
 	if ((rb->size - rb->write_index) > count)
@@ -134,8 +142,7 @@ mr_size_t mr_ringbuffer_write(mr_ringbuffer_t rb, const mr_uint8_t *buffer, mr_s
 		mr_memcpy(&rb->buffer[rb->write_index], buffer, count);
 		rb->write_index += count;
 
-		mr_hw_interrupt_disable();
-
+		mr_hw_interrupt_enable();
 		return count;
 	}
 
@@ -145,8 +152,7 @@ mr_size_t mr_ringbuffer_write(mr_ringbuffer_t rb, const mr_uint8_t *buffer, mr_s
 	rb->write_mirror = ~ rb->write_mirror;
 	rb->write_index = count - (rb->size - rb->write_index);
 
-	mr_hw_interrupt_disable();
-
+	mr_hw_interrupt_enable();
 	return count;
 }
 
@@ -170,6 +176,8 @@ mr_size_t mr_ringbuffer_write_force(mr_ringbuffer_t rb, const mr_uint8_t *buffer
 		count = rb->size;
 	}
 
+	mr_hw_interrupt_disable();
+
 	/* Copy the data from the buffer to the ringbuffer */
 	if ((rb->size - rb->write_index) > count)
 	{
@@ -178,8 +186,7 @@ mr_size_t mr_ringbuffer_write_force(mr_ringbuffer_t rb, const mr_uint8_t *buffer
 		if (count > length)
 			rb->read_index = rb->write_index;
 
-		mr_hw_interrupt_disable();
-
+		mr_hw_interrupt_enable();
 		return count;
 	}
 
@@ -197,8 +204,7 @@ mr_size_t mr_ringbuffer_write_force(mr_ringbuffer_t rb, const mr_uint8_t *buffer
 		rb->read_index = rb->write_index;
 	}
 
-	mr_hw_interrupt_disable();
-
+	mr_hw_interrupt_enable();
 	return count;
 }
 
