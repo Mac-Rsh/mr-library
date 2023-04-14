fbced89c77a74452f5f6a3592fc1da7460945b27
MacRsh <1063220965@qq.com>
2023-04-14 18:15:35 +0800

    优化: 1.事件驱动框架改用AVL提高分发效率。

--- src/event.c
+++ src/event.c
@@ -12,202 +12,178 @@
 
 mr_event_manager_t mr_event_manager_find(const char *name)
 {
-	mr_event_manager_t event_manager = MR_NULL;
+	mr_event_manager_t manager = MR_NULL;
 
-	/* Find the event-manager object from the device container */
-	event_manager = (mr_event_manager_t)mr_object_find(name, MR_CONTAINER_TYPE_EVENT);
+	MR_ASSERT(name != MR_NULL);
 
-	return event_manager;
+	/* Find the manager object from the device container */
+	manager = (mr_event_manager_t)mr_object_find(name, MR_CONTAINER_TYPE_EVENT);
+
+	return manager;
 }
 
-mr_err_t mr_event_manager_add_to_container(mr_event_manager_t event_manager,
+mr_err_t mr_event_manager_add_to_container(mr_event_manager_t manager,
 										   const char *name,
-										   mr_uint8_t *queue_pool,
-										   mr_size_t queue_pool_size)
+										   enum mr_event_manager_type type,
+										   mr_uint8_t *pool,
+										   mr_size_t pool_size)
 {
 	mr_err_t ret = MR_ERR_OK;
 
-	MR_ASSERT(event_manager != MR_NULL);
-	MR_ASSERT(queue_pool != MR_NULL);
-	MR_ASSERT(queue_pool_size >= 2);
+	MR_ASSERT(manager != MR_NULL);
+	MR_ASSERT(pool != MR_NULL);
+	MR_ASSERT(pool_size >= sizeof(mr_uint32_t));
 
 	/* Add the object to the container */
-	ret = mr_object_add_to_container(&event_manager->object, name, MR_CONTAINER_TYPE_EVENT);
+	ret = mr_object_add_to_container(&manager->object, name, MR_CONTAINER_TYPE_EVENT);
 	if (ret != MR_ERR_OK)
 		return ret;
 
 	/* Initialize the private fields */
-	mr_list_init(&event_manager->list);
-	mr_ringbuffer_init(&event_manager->queue, queue_pool, queue_pool_size);
+	manager->type = type;
+	manager->avl = MR_NULL;
+	mr_ringbuffer_init(&manager->queue, pool, pool_size);
 
 	return MR_ERR_OK;
 }
 
-mr_err_t mr_event_manager_remove_from_container(mr_event_manager_t event_manager)
+mr_err_t mr_event_manager_remove_from_container(mr_event_manager_t manager)
 {
 	mr_err_t ret = MR_ERR_OK;
-	mr_size_t length = 0;
 
-	MR_ASSERT(event_manager != MR_NULL);
+	MR_ASSERT(manager != MR_NULL);
 
-	length = mr_list_get_length(&event_manager->list);
-	if (length != 0)
+	/* Check the manager list for events */
+	if (manager->avl != MR_NULL)
 		return - MR_ERR_BUSY;
 
 	/* Remove the object from the container */
-	ret = mr_object_remove_from_container(&event_manager->object);
+	ret = mr_object_remove_from_container(&manager->object);
 	if (ret != MR_ERR_OK)
 		return ret;
 
 	/* Reset the private fields */
-	mr_list_init(&event_manager->list);
-	mr_ringbuffer_reset(&event_manager->queue);
+	manager->avl = MR_NULL;
+	mr_ringbuffer_reset(&manager->queue);
 
 	return MR_ERR_OK;
 }
 
-mr_err_t mr_event_manager_notify(mr_event_manager_t event_manager, mr_uint16_t value)
+mr_err_t mr_event_manager_notify(mr_event_manager_t manager, mr_uint32_t value)
 {
-	mr_size_t send_count = 0;
+	mr_size_t send_size = 0;
 
-	send_count = mr_ringbuffer_write(&event_manager->queue, (mr_uint8_t *)&value, 2);
-	if (send_count != 2)
+	MR_ASSERT(manager != MR_NULL);
+
+	send_size = mr_ringbuffer_write(&manager->queue, (mr_uint8_t *)&value, sizeof(value));
+	if (send_size != sizeof(value))
 		return - MR_ERR_BUSY;
 
 	return MR_ERR_OK;
 }
 
-mr_err_t mr_event_manager_process(mr_event_manager_t event_manager)
+mr_err_t mr_event_manager_process(mr_event_manager_t manager)
 {
-	mr_list_t list = MR_NULL;
+	mr_avl_t node = MR_NULL;
 	mr_event_t event = MR_NULL;
-	mr_uint16_t value = 0;
+	mr_uint32_t value = 0;
 
-	MR_ASSERT(event_manager != MR_NULL);
+	MR_ASSERT(manager != MR_NULL);
 
-	while (mr_ringbuffer_get_data_length(&event_manager->queue) >= 2)
+	while (mr_ringbuffer_get_data_length(&manager->queue) >= sizeof(value))
 	{
-		mr_ringbuffer_read(&event_manager->queue, (mr_uint8_t *)&value, 2);
+		mr_ringbuffer_read(&manager->queue, (mr_uint8_t *)&value, sizeof(value));
 
-		/* Walk through the event-manager looking for event */
-		for (list = (event_manager->list).next; list != &(event_manager->list); list = list->next)
-		{
-			event = mr_container_of(list, struct mr_event, list);
-			if (event->value == value)
-			{
-				event->callback(event, event->args);
-				break;
-			}
-		}
+		node = mr_avl_find(manager->avl, value);
+		if (node == MR_NULL)
+			continue;
+
+		event = mr_struct_of(node, struct mr_event, avl);
+		event->callback(manager, event->args);
 	}
 
 	return MR_ERR_OK;
 }
 
-mr_event_t mr_event_find(mr_event_manager_t event_manager, mr_uint16_t event_value)
+mr_event_t mr_event_find(mr_event_manager_t manager, mr_uint32_t value)
 {
-	mr_list_t list = MR_NULL;
-	mr_event_t event = MR_NULL;
+	MR_ASSERT(manager != MR_NULL);
 
-	MR_ASSERT(event_manager != MR_NULL);
-
-	mr_hw_interrupt_disable();
-
-	/* Walk through the event-manager looking for event */
-	for (list = (event_manager->list).next; list != &(event_manager->list); list = list->next)
-	{
-		event = mr_container_of(list, struct mr_event, list);
-		if (event->value == event_value)
-		{
-			mr_hw_interrupt_enable();
-			return event;
-		}
-	}
-
-	mr_hw_interrupt_enable();
-	return MR_NULL;
+	return (mr_event_t)mr_avl_find(manager->avl, value);
 }
 
-mr_err_t mr_event_add_to_manager(mr_event_manager_t event_manager,
+mr_err_t mr_event_add_to_manager(mr_event_manager_t manager,
 								 mr_event_t event,
-								 mr_uint16_t value,
-								 mr_err_t (*callback)(mr_event_t event, void *args), void *args)
+								 mr_uint32_t value,
+								 mr_err_t (*callback)(mr_event_manager_t event_manager, void *args), void *args)
 {
-	MR_ASSERT(event_manager != MR_NULL);
+	MR_ASSERT(manager != MR_NULL);
 	MR_ASSERT(event != MR_NULL);
 	MR_ASSERT(callback != MR_NULL);
 
-	/* Check if the object already exists in the container */
-	if (mr_event_find(event_manager, value) != MR_NULL)
+	/* Check if the event already exists in the manager */
+	if (mr_event_find(manager, value) != MR_NULL)
 		return - MR_ERR_GENERIC;
 
-	event->value = value;
+	mr_avl_init(&event->avl, value);
 	event->callback = callback;
 	event->args = args;
 
+	/* Disable interrupt */
 	mr_hw_interrupt_disable();
 
-	/* Insert the event into the event-manager's list */
-	mr_list_insert_after(&(event_manager->list), &(event->list));
+	/* Insert the event into the manager's list */
+	mr_avl_insert(&manager->avl, &event->avl);
 
+	/* Enable interrupt */
 	mr_hw_interrupt_enable();
+
 	return MR_ERR_OK;
 }
 
-mr_err_t mr_event_remove_from_manager(mr_event_t event)
+mr_err_t mr_event_remove_from_manager(mr_event_manager_t manager, mr_event_t event)
 {
 	MR_ASSERT(event != MR_NULL);
 
+	/* Disable interrupt */
 	mr_hw_interrupt_disable();
 
-	/* Remove the event from the event-manager's list */
-	mr_list_remove(&(event->list));
+	/* Remove the event from the manager's list */
+	mr_avl_remove(&manager->avl, &event->avl);
 
+	/* Enable interrupt */
 	mr_hw_interrupt_enable();
+
 	return MR_ERR_OK;
 }
 
-mr_err_t mr_event_create_to_manager(mr_event_manager_t event_manager,
-									mr_uint16_t value,
-									mr_err_t (*callback)(mr_event_t event, void *args),
+mr_err_t mr_event_create_to_manager(mr_event_manager_t manager,
+									mr_uint32_t value,
+									mr_err_t (*callback)(mr_event_manager_t event_manager, void *args),
 									void *args)
 {
 	mr_event_t event = MR_NULL;
 
-	MR_ASSERT(event_manager != MR_NULL);
+	MR_ASSERT(manager != MR_NULL);
 	MR_ASSERT(callback != MR_NULL);
 
-	/* Check if the object already exists in the container */
-	if (mr_event_find(event_manager, value) != MR_NULL)
+	/* Check if the event already exists in the manager */
+	if (mr_event_find(manager, value) != MR_NULL)
 		return - MR_ERR_GENERIC;
 
-	event = (mr_event_t)mr_malloc(sizeof(struct mr_event *));
+	event = (mr_event_t)mr_malloc(sizeof(struct mr_event));
 	if (event == MR_NULL)
 		return - MR_ERR_NO_MEMORY;
 
-	event->value = value;
-	event->callback = callback;
-	event->args = args;
-
-	mr_hw_interrupt_disable();
-
-	/* Insert the event into the event-manager's list */
-	mr_list_insert_after(&(event_manager->list), &(event->list));
-
-	mr_hw_interrupt_enable();
-	return MR_ERR_OK;
+	return mr_event_add_to_manager(manager, event, value, callback, args);
 }
 
-mr_err_t mr_event_delete_from_manager(mr_event_t event)
+mr_err_t mr_event_delete_from_manager(mr_event_manager_t manager, mr_event_t event)
 {
 	MR_ASSERT(event != MR_NULL);
 
-	mr_hw_interrupt_disable();
-
-	/* Remove the event from the event-manager's list */
-	mr_list_remove(&(event->list));
-
-	mr_hw_interrupt_enable();
+	/* Remove the event from the manager's list */
+	mr_event_remove_from_manager(manager, event);
 
 	mr_free(event);
 
