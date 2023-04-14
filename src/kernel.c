4170a9346f73872416fa4d6b006b48e756937614
MacRsh <1063220965@qq.com>
2023-04-14 18:13:02 +0800

    优化: 1.优化代码及注释。

--- src/kernel.c
+++ src/kernel.c
@@ -29,6 +29,13 @@ static struct mr_container mr_kernel_container[_MR_CONTAINER_TYPE_MASK] =
 		},
 	};
 
+/**
+ * This function find the container.
+ *
+ * @param type The flag of the container.
+ *
+ * @return a handle to the find container, or MR_NULL if not find.
+ */
 mr_container_t mr_container_find(enum mr_container_type type)
 {
 	MR_ASSERT(type < _MR_CONTAINER_TYPE_MASK);
@@ -36,6 +43,14 @@ mr_container_t mr_container_find(enum mr_container_type type)
 	return &mr_kernel_container[type];
 }
 
+/**
+ * This function find the object.
+ *
+ * @param name The name of the object.
+ * @param type The container flag to which the object belongs.
+ *
+ * @return a handle to the find object, or MR_NULL if not find.
+ */
 mr_object_t mr_object_find(const char *name, enum mr_container_type type)
 {
 	mr_list_t list = MR_NULL;
@@ -45,24 +60,36 @@ mr_object_t mr_object_find(const char *name, enum mr_container_type type)
 	/* Get corresponding container */
 	container = mr_container_find(type);
 
+	/* Disable interrupt */
 	mr_hw_interrupt_disable();
 
 	/* Walk through the container looking for objects */
 	for (list = (container->list).next; list != &(container->list); list = list->next)
 	{
-		object = mr_container_of(list, struct mr_object, list);
+		object = mr_struct_of(list, struct mr_object, list);
 		if (mr_strncmp(object->name, name, MR_NAME_MAX) == 0)
 		{
+			/* Enable interrupt */
 			mr_hw_interrupt_enable();
 			return object;
 		}
 	}
 
+	/* Enable interrupt */
 	mr_hw_interrupt_enable();
 
 	return MR_NULL;
 }
 
+/**
+ * This function add object to the container.
+ *
+ * @param object The object to be added.
+ * @param name The name of the object.
+ * @param type The target container flag.
+ *
+ * @return MR_ERR_OK on success, otherwise an error code.
+ */
 mr_err_t mr_object_add_to_container(mr_object_t object, const char *name, enum mr_container_type container_type)
 {
 	mr_container_t container = MR_NULL;
@@ -71,7 +98,7 @@ mr_err_t mr_object_add_to_container(mr_object_t object, const char *name, enum m
 	MR_ASSERT(name != MR_NULL);
 
 	/* Check if the object is already registered */
-	if (object->type & MR_OBJECT_TYPE_REGISTER)
+	if (object->flag & MR_OBJECT_TYPE_REGISTER)
 		return - MR_ERR_BUSY;
 
 	/* Check if the object already exists in the container */
@@ -81,37 +108,58 @@ mr_err_t mr_object_add_to_container(mr_object_t object, const char *name, enum m
 	/* Copy the specified name to the object name */
 	mr_strncpy(object->name, name, MR_NAME_MAX);
 
-	/* Find the container for the specified type */
+	/* Find the container for the specified flag */
 	container = mr_container_find(container_type);
 
+	/* Disable interrupt */
 	mr_hw_interrupt_disable();
 
 	/* Insert the object into the container's list */
 	mr_list_insert_after(&(container->list), &(object->list));
-	object->type |= MR_OBJECT_TYPE_REGISTER;
+	object->flag |= MR_OBJECT_TYPE_REGISTER;
 
+	/* Enable interrupt */
 	mr_hw_interrupt_enable();
+
 	return MR_ERR_OK;
 }
 
+/**
+ * This function remove object from the container.
+ *
+ * @param object The object to be removed.
+ *
+ * @return MR_ERR_OK on success, otherwise an error code.
+ */
 mr_err_t mr_object_remove_from_container(mr_object_t object)
 {
 	MR_ASSERT(object != MR_NULL);
 
 	/* Check if the object is registered */
-	if ((object->type & MR_OBJECT_TYPE_REGISTER) == 0)
+	if ((object->flag & MR_OBJECT_TYPE_REGISTER) == 0)
 		return - MR_ERR_GENERIC;
 
+	/* Disable interrupt */
 	mr_hw_interrupt_disable();
 
 	/* Remove the object from the container's list */
 	mr_list_remove(&(object->list));
-	object->type &= ~ MR_OBJECT_TYPE_REGISTER;
+	object->flag &= ~ MR_OBJECT_TYPE_REGISTER;
 
+	/* Enable interrupt */
 	mr_hw_interrupt_enable();
+
 	return MR_ERR_OK;
 }
 
+/**
+ * This function move object to specified container.
+ *
+ * @param object The object to be moved.
+ * @param dst_type The target container flag.
+ *
+ * @return MR_ERR_OK on success, otherwise an error code.
+ */
 mr_err_t mr_object_move(mr_object_t object, enum mr_container_type dst_type)
 {
 	mr_err_t ret = MR_ERR_OK;
@@ -129,6 +177,12 @@ mr_err_t mr_object_move(mr_object_t object, enum mr_container_type dst_type)
 	return ret;
 }
 
+/**
+ * This function rename object.
+ *
+ * @param object The object to be renamed.
+ * @param name The name of the object.
+ */
 void mr_object_rename(mr_object_t object, char *name)
 {
 	MR_ASSERT(object != MR_NULL);
@@ -136,6 +190,11 @@ void mr_object_rename(mr_object_t object, char *name)
 	mr_strncpy(object->name, name, MR_NAME_MAX);
 }
 
+/**
+ * This function initialize mutex.
+ *
+ * @param mutex The mutex to be initialized.
+ */
 void mr_mutex_init(mr_mutex_t mutex)
 {
 	MR_ASSERT(mutex != MR_NULL);
@@ -144,18 +203,29 @@ void mr_mutex_init(mr_mutex_t mutex)
 	mutex->lock = MR_UNLOCK;
 }
 
+/**
+ * This function take the mutex.
+ *
+ * @param mutex The mutex to be taken.
+ * @param owner The object that take the mutex.
+ *
+ * @return MR_ERR_OK on success, otherwise an error code.
+ */
 mr_err_t mr_mutex_take(mr_mutex_t mutex, mr_object_t owner)
 {
 	MR_ASSERT(mutex != MR_NULL);
 	MR_ASSERT(owner != MR_NULL);
 
+	/* Disable interrupt */
 	mr_hw_interrupt_disable();
 
 	if (mutex->owner != owner)
 	{
 		if (mutex->lock == MR_LOCK)
 		{
+			/* Enable interrupt */
 			mr_hw_interrupt_enable();
+
 			return - MR_ERR_BUSY;
 		}
 
@@ -163,25 +233,40 @@ mr_err_t mr_mutex_take(mr_mutex_t mutex, mr_object_t owner)
 	}
 	mutex->lock = MR_LOCK;
 
+	/* Enable interrupt */
 	mr_hw_interrupt_enable();
+
 	return MR_ERR_OK;
 }
 
+/**
+ * This function release the mutex.
+ *
+ * @param mutex The mutex to be released.
+ * @param owner The object that release the mutex.
+ *
+ * @return MR_ERR_OK on success, otherwise an error code.
+ */
 mr_err_t mr_mutex_release(mr_mutex_t mutex, mr_object_t owner)
 {
 	MR_ASSERT(mutex != MR_NULL);
 	MR_ASSERT(owner != MR_NULL);
 
+	/* Disable interrupt */
 	mr_hw_interrupt_disable();
 
 	if (mutex->owner == owner)
 	{
 		mutex->lock = MR_UNLOCK;
 
+		/* Enable interrupt */
 		mr_hw_interrupt_enable();
+
 		return MR_ERR_OK;
 	}
 
+	/* Enable interrupt */
 	mr_hw_interrupt_enable();
+
 	return - MR_ERR_GENERIC;
 }
