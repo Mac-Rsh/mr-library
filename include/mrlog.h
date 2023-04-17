a16839a8435e544744d08ecfa8e987d5d584402a
MacRsh <1063220965@qq.com>
2023-04-18 01:08:16 +0800

    新增: 1.内核日志，支持分级输出。

--- include/mrlog.h
+++ include/mrlog.h
@@ -13,21 +13,28 @@
 
 #include <mrdef.h>
 
-MR_INLINE void mr_assert_handler()
-{
-	while (1)
-	{
+#define MR_LOG_A(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_ASSERT, __FUNCTION__, FORMAT, ##__VA_ARGS__)
+#define MR_LOG_E(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_ERROR, __FUNCTION__, FORMAT, ##__VA_ARGS__)
+#define MR_LOG_W(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_WARNING, __FUNCTION__, FORMAT, ##__VA_ARGS__)
+#define MR_LOG_I(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_INFO, __FUNCTION__, FORMAT, ##__VA_ARGS__)
+#define MR_LOG_D(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_DEBUG, __FUNCTION__, FORMAT, ##__VA_ARGS__)
 
-	}
-}
-
-#define MR_ASSERT(EX)           \
-    do{                         \
-        if (!(EX))              \
-        {                       \
-          mr_printf("ASSERT: [%s] [%d]\r\n", __FUNCTION__ , __LINE__);                      \
-          mr_assert_handler();  \
-        }                       \
+#if (MR_LOG_ASSERT == MR_CONF_ENABLE)
+#define MR_ASSERT(EX)            			 \
+    do{                          			 \
+        if (!(EX))               			 \
+        {                        			 \
+          MR_LOG_A("File: %s, Line: %d\r\n", \
+		  		   __FILE__,                 \
+		  		   __LINE__);                \
+          mr_assert_handler();   		 	 \
+        }                        			 \
     }while(0)
+#else
+#define MR_ASSERT(EX)
+#endif
+
+void mr_log_output(mr_base_t level, const char *tag, const char *format, ...);
+void mr_assert_handler(void);
 
 #endif
---
+++ src/log.c
@@ -0,0 +1,248 @@
+/*
+ * Copyright (c), mr-library Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-04-17     MacRsh       first version
+ */
+
+#include <mrlib.h>
+
+static char *log_level_name[] =
+	{
+		"LOG_A",
+		"LOG_E",
+		"LOG_W",
+		"LOG_I",
+		"LOG_D",
+	};
+
+static mr_size_t _mr_log_output(const char *fmt, va_list args)
+{
+	mr_size_t rst = 0;
+	char buffer[MR_LOG_BUFSZ];
+	char value_buffer[12];
+	char *string;
+	mr_int32_t value;
+	mr_uint32_t u_value, bits, number;
+	mr_double_t d_value;
+
+	while (*fmt != '\0')
+	{
+		if (*fmt == '%')
+		{
+			fmt ++;
+
+			if (*fmt == '.')
+			{
+				fmt ++;
+				number = (mr_uint32_t)(*fmt - '0');
+				if (number > 6)
+					number = 6;
+				fmt ++;
+			} else
+				number = 6;
+
+			/* Dispose %d,%x,%o,%u,%s,%c,%f */
+			switch (*fmt)
+			{
+				/* DEC */
+				case 'd':
+				{
+					value = va_arg(args, int);
+					if (value < 0)
+					{
+						value = - value;
+						buffer[rst ++] = '-';
+					}
+
+					bits = 0;
+					do
+					{
+						value_buffer[bits ++] = '0' + value % 10;
+						value /= 10;
+					} while (value);
+
+					while (bits)
+					{
+						buffer[rst ++] = value_buffer[-- bits];
+					}
+					break;
+				}
+
+					/* HEX */
+				case 'x':
+				case 'X':
+				{
+					u_value = va_arg(args, unsigned int);
+
+					bits = 0;
+					do
+					{
+						value_buffer[bits] = '0' + u_value % 16;
+
+						if (value_buffer[bits] > '9')
+							value_buffer[bits] = 'A' + (value_buffer[bits] - '9' - 1);
+
+						u_value /= 16;
+						bits ++;
+					} while (u_value);
+
+					while (bits)
+					{
+						buffer[rst ++] = value_buffer[-- bits];
+					}
+					break;
+				}
+
+					/* OCT */
+				case 'o':
+				{
+					u_value = va_arg(args, unsigned int);
+
+					bits = 0;
+					do
+					{
+						value_buffer[bits] = '0' + u_value % 8;
+
+						if (value_buffer[bits] > '9')
+							value_buffer[bits] = 'A' + (value_buffer[bits] - '9' - 1);
+
+						u_value /= 8;
+						bits ++;
+					} while (u_value);
+
+					while (bits)
+					{
+						buffer[rst ++] = value_buffer[-- bits];
+					}
+					break;
+				}
+
+					/* Unsigned DEC */
+				case 'u':
+				{
+					u_value = va_arg(args, int);
+
+					bits = 0;
+					do
+					{
+						value_buffer[bits ++] = '0' + u_value % 10;
+						u_value /= 10;
+					} while (u_value);
+
+					while (bits)
+					{
+						buffer[rst ++] = value_buffer[-- bits];
+					}
+					break;
+				}
+
+				case 'c':
+				{
+					buffer[rst ++] = va_arg(args, int);
+					break;
+				}
+
+				case 's':
+				{
+					string = va_arg(args, char *);
+
+					while (*string != '\0')
+					{
+						buffer[rst ++] = *string;
+						string ++;
+					}
+					break;
+				}
+
+#if (MR_PRINTF_FLOAT == MR_CONF_ENABLE)
+				case 'f':
+				{
+					d_value = va_arg(args, double);
+					if (d_value < 0)
+					{
+						d_value = - d_value;
+						buffer[rst ++] = '-';
+					}
+
+					value = (mr_int32_t)d_value;
+					d_value -= value;
+
+					bits = 0;
+					do
+					{
+						value_buffer[bits ++] = '0' + value % 10;
+						value /= 10;
+					} while (value);
+
+					while (bits)
+					{
+						buffer[rst ++] = value_buffer[-- bits];
+					}
+
+					buffer[rst ++] = '.';
+					value = (int)((d_value * 1000000.0f) + 0.5f);
+
+					bits = 0;
+					do
+					{
+						value_buffer[bits ++] = '0' + value % 10;
+						value /= 10;
+					} while (value);
+
+					while (number)
+					{
+						buffer[rst ++] = value_buffer[-- bits];
+						number --;
+					}
+					break;
+				}
+#endif
+			}
+
+			fmt ++;
+		} else
+		{
+			buffer[rst ++] = *fmt;
+			fmt ++;
+		}
+	}
+
+	buffer[rst] = '\0';
+	printf("%s", buffer);
+
+	return rst;
+}
+
+void mr_log_output(mr_base_t level, const char *tag, const char *format, ...)
+{
+	va_list args;
+
+#if (MR_LOG_OUTPUT == MR_CONF_DISABLE)
+	return;
+#endif
+
+	if (level > MR_LOG_LEVEL || level < 0)
+		return;
+
+	va_start(args, format);
+
+	mr_printf("[%s/%s]: ", log_level_name[level], tag);
+	_mr_log_output(format, args);
+
+	va_end(args);
+}
+
+void mr_assert_handler(void)
+{
+	while (1)
+	{
+
+	}
+}
+
+
+
