0a6e9097d8e54fcd2913275705265775802be9c8
MacRsh <1063220965@qq.com>
2023-04-20 02:32:25 +0800

    优化: 1.log输出添加tag标签。

--- include/mrlog.h
+++ include/mrlog.h
@@ -13,18 +13,19 @@
 
 #include <mrdef.h>
 
-#define MR_LOG_A(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_ASSERT, __FUNCTION__, FORMAT, ##__VA_ARGS__)
-#define MR_LOG_E(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_ERROR, __FUNCTION__, FORMAT, ##__VA_ARGS__)
-#define MR_LOG_W(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_WARNING, __FUNCTION__, FORMAT, ##__VA_ARGS__)
-#define MR_LOG_I(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_INFO, __FUNCTION__, FORMAT, ##__VA_ARGS__)
-#define MR_LOG_D(FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_DEBUG, __FUNCTION__, FORMAT, ##__VA_ARGS__)
+#define MR_LOG_A(TAG, FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_ASSERT, TAG, FORMAT, ##__VA_ARGS__)
+#define MR_LOG_E(TAG, FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_ERROR, TAG, FORMAT, ##__VA_ARGS__)
+#define MR_LOG_W(TAG, FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_WARNING, TAG, FORMAT, ##__VA_ARGS__)
+#define MR_LOG_I(TAG, FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_INFO, TAG, FORMAT, ##__VA_ARGS__)
+#define MR_LOG_D(TAG, FORMAT, ...)    mr_log_output(MR_LOG_LEVEL_DEBUG, TAG, FORMAT, ##__VA_ARGS__)
 
 #if (MR_LOG_ASSERT == MR_CONF_ENABLE)
 #define MR_ASSERT(EX)            			 \
     do{                          			 \
         if (!(EX))               			 \
         {                        			 \
-          MR_LOG_A("File: %s, Line: %d\r\n", \
+          MR_LOG_A(__FUNCTION__,     		 \
+		           "File: %s, Line: %d\r\n", \
 		  		   __FILE__,                 \
 		  		   __LINE__);                \
           mr_assert_handler();   		 	 \
