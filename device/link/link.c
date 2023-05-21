be8088d8fd26ef3ba0180f9819d70ad967afbdb0
MacRsh <1063220965@qq.com>
2023-05-22 01:32:49 +0800

    1.修改fifo传参类型。

--- device/link/link.c
+++ device/link/link.c
@@ -1,184 +1,124 @@
 /*
- * Copyright (c), mr-library Development Team
+ * Copyright (c) 2023, mr-library Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2023-03-10     MacRsh       first version
+ * 2023-04-23     MacRsh       first version
  */
 
-#include "link.h"
+#include <device/link/link.h>
 
-static mr_uint32_t mr_link_get_crc(const mr_uint8_t *data, mr_size_t count)
+#undef LOG_TAG
+#define LOG_TAG "link"
+
+mr_link_t mr_link_find(const char *name)
 {
-	mr_uint32_t crc = 0;
+	MR_ASSERT(name != MR_NULL);
 
-	while (count --)
+	/* Find the link object from the misc-container */
+	return (mr_link_t)mr_object_find(name, MR_CONTAINER_TYPE_MISC);
+}
+
+mr_err_t mr_link_add(mr_link_t link,
+					 const char *name,
+					 mr_size_t rx_bufsz,
+					 mr_uint32_t (*get_crc)(mr_uint8_t *buffer, mr_size_t length))
+{
+	mr_err_t ret = MR_ERR_OK;
+	mr_uint8_t *pool = MR_NULL;
+
+	MR_ASSERT(link != MR_NULL);
+
+	/* Add the object to the container */
+	ret = mr_object_add(&link->object, name, MR_CONTAINER_TYPE_MISC);
+	if (ret != MR_ERR_OK)
+		return ret;
+
+	/* Initialize the private fields */
+	pool = (mr_uint8_t *)malloc(rx_bufsz);
+	if (pool == NULL)
 	{
-		crc += *data;
-		data ++;
+		MR_LOG_D(LOG_TAG, "Failed to allocate memory for rx-pool\r\n");
+		return - MR_ERR_NO_MEMORY;
+	}
+	mr_fifo_init(&link->rx_fifo, pool, rx_bufsz);
+	link->tx_sequence = 0;
+	link->tx_packet = MR_NULL;
+	link->get_crc = get_crc;
+
+	return MR_ERR_OK;
+}
+
+mr_err_t mr_link_remove(mr_link_t link)
+{
+	mr_err_t ret = MR_ERR_OK;
+
+	MR_ASSERT(link != MR_NULL);
+
+}
+
+mr_uint16_t mr_link_get_crc(mr_uint8_t *buffer, mr_uint16_t length)
+{
+	mr_uint16_t crc = 0;
+
+	while (length --)
+	{
+		crc += *buffer ++;
 	}
 
 	return crc;
 }
 
-static mr_err_t mr_link_callback(mr_device_t device, void *args)
+mr_err_t mr_link_packet_create(mr_uint8_t *buffer, mr_uint16_t length, mr_link_t link)
 {
-	static struct mr_link_packet_head packet_head = {MR_NULL};
-	static enum mr_link_state state = MR_LINK_CHECK_HEAD;
-	mr_uint8_t buffer[MR_LINK_PACKET_MAX] = {0};
+	mr_link_packet_t packet = MR_NULL;
 
-	switch (state)
+	MR_ASSERT(buffer != NULL);
+
+	packet = (mr_link_packet_t)malloc(sizeof(struct mr_link_packet));
+	if (packet == NULL)
 	{
-		case MR_LINK_CHECK_HEAD:
-		{
-			if (*(mr_size_t *)args >= sizeof(packet_head))
-			{
-				/* Check frame-head */
-				mr_device_read(device, - 1, &packet_head, 1);
-				if (packet_head.frame_head != 0x3A)
-					return - MR_ERR_BUSY;
-
-				mr_device_read(device, - 1, (((mr_uint8_t *)&packet_head) + 1), sizeof(packet_head) - 1);
-				state = MR_LINK_CHECK_DATA;
-				return MR_ERR_OK;
-			} else
-				return - MR_ERR_BUSY;
-		}
-
-		case MR_LINK_CHECK_DATA:
-		{
-			if (*(mr_size_t *)args >= packet_head.length)
-			{
-				mr_device_read(device, - 1, buffer, packet_head.length);
-
-				if (packet_head.crc == mr_link_get_crc(buffer, packet_head.length))
-				{
-					for (int i = 0; i < packet_head.length; ++ i)
-					{
-						printf("%x\r\n", buffer[i]);
-					}
-				}
-			} else
-				return - MR_ERR_BUSY;
-		}
-
-		default:return - MR_ERR_BUSY;
-	}
-}
-
-static mr_device_t mr_link_open_port(const char *port_name)
-{
-	mr_err_t error_code = MR_ERR_OK;
-	mr_device_t port_device = MR_NULL;
-
-	port_device = mr_device_find(port_name);
-	if (port_device == MR_NULL)
-		return MR_NULL;
-
-	error_code = mr_device_open(port_device, MR_OPEN_RDWR);
-	if (error_code != MR_ERR_OK)
-		return MR_NULL;
-
-	mr_device_ioctl(port_device, MR_CMD_SET_RX_CALLBACK, mr_link_callback);
-
-	return port_device;
-}
-
-static void mr_link_packet_head_init(struct mr_link_packet_head *packet_head, mr_uint8_t src_addr, mr_uint8_t dst_addr)
-{
-	packet_head->frame_head = 0x3A;
-	packet_head->crc = 0;
-	packet_head->ack = 0;
-	packet_head->sequence = 0;
-	packet_head->sequence = 0;
-	packet_head->src_addr = src_addr;
-	packet_head->dst_addr = dst_addr;
-	packet_head->length = 0;
-}
-
-mr_size_t mr_link_pack_head(struct mr_link_packet_head *head,
-							mr_uint8_t *buffer,
-							mr_size_t count)
-{
-	if (count > MR_LINK_PACKET_MAX)
-		count = MR_LINK_PACKET_MAX;
-
-	head->sequence ++;
-	head->length = count;
-	head->crc = mr_link_get_crc(buffer, head->length);
-
-	return count;
-}
-
-mr_size_t mr_link_write(mr_link_t link,
-						mr_uint8_t src_addr,
-						mr_uint8_t dst_addr,
-						mr_uint8_t *buffer,
-						mr_size_t count)
-{
-	mr_device_t port_device = (mr_device_t)link->device.data;
-	struct mr_link_packet_head packet_head = {MR_NULL};
-	mr_size_t send_count = 0;
-
-	mr_link_packet_head_init(&packet_head, src_addr, dst_addr);
-
-	while (send_count < count)
-	{
-		mr_link_pack_head(&packet_head, &buffer[send_count], (count - send_count));
-
-		mr_device_write(port_device, - 1, (mr_uint8_t *)&packet_head, sizeof(struct mr_link_packet_head));
-		mr_device_write(port_device, - 1, &buffer[send_count], packet_head.length);
-
-		send_count += packet_head.length;
+		MR_LOG_D(LOG_TAG, "Failed to allocate memory for packet\r\n");
+		return - MR_ERR_NO_MEMORY;
 	}
 
-	return count;
-}
+	packet->head.start = MR_LINK_PACKET_HEAD;
+	packet->head.length = length;
+	packet->head.sequence = link->tx_sequence;
 
-mr_err_t mr_link_service_add_to_container(mr_link_service_t link_service, const char *name)
-{
-	mr_err_t error_code = MR_ERR_OK;
-	const static struct mr_device_ops device_ops = {MR_NULL};
+	packet->data = (mr_uint8_t *)malloc(length);
+	if (packet->data == NULL)
+	{
+		MR_LOG_D(LOG_TAG, "Failed to allocate memory for packet\r\n");
+		return - MR_ERR_NO_MEMORY;
+	}
+	memcpy(packet->data, buffer, length);
 
-	/* Initialize the device and add the device to the container */
-	mr_device_init(&link_service->device, name);
-	error_code = mr_device_add_to_container(&link_service->device,
-											MR_DEVICE_TYPE_LINK_SERVICE,
-											MR_OPEN_RDWR,
-											&device_ops,
-											MR_NULL);
-	if (error_code != MR_ERR_OK)
-		return error_code;
+	packet->head.crc = link->get_crc(packet->data, length);
 
-	/* Initialize the test_link-service fields */
-
+	link->tx_sequence ++;
+	if (link->tx_packet == MR_NULL)
+		link->tx_packet = packet;
+	else
+		link->tx_packet->next = packet;
 
 	return MR_ERR_OK;
 }
 
-mr_err_t mr_link_add_to_container(mr_link_t link, const char *name, char *port_name)
+mr_err_t mr_link_packet_parse(mr_link_packet_t packet, mr_link_t link)
 {
-	mr_err_t error_code = MR_ERR_OK;
-	const static struct mr_device_ops device_ops = {MR_NULL};
-	mr_device_t port_device = MR_NULL;
+	MR_ASSERT(packet != NULL);
+	MR_ASSERT(link != NULL);
 
-	/* Open service port */
-	port_device = mr_link_open_port(port_name);
-	if (port_device == MR_NULL)
-		return - MR_ERR_NOT_FOUND;
+//	if (packet->head.start == m)
 
-	/* Initialize the device and add the device to the container */
-	mr_device_init(&link->device, name);
-	error_code = mr_device_add_to_container(&link->device, MR_DEVICE_TYPE_LINK, MR_OPEN_RDWR, &device_ops, port_device);
-	if (error_code != MR_ERR_OK)
-		return error_code;
-
-	/* Initialize the link fields */
-	mr_list_init(&link->list);
-
-	return MR_ERR_OK;
 }
 
+mr_uint16_t mr_link_parse_packet(mr_link_packet_t packet, mr_uint8_t *buffer)
+{
+	MR_ASSERT(packet != NULL);
+	MR_ASSERT(buffer != NULL);
 
+}
