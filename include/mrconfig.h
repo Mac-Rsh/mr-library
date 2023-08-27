323d61b4700f71841708508292dea49645c54d8f
MacRsh <MacRsh@outlook.com>
2023-08-27 15:14:44 +0800

    1.重构配置文件（暂未支持timer和pwm）。

--- include/mrconfig.h
+++ include/mrconfig.h
@@ -3,74 +3,160 @@
 
 #include "mrboard.h"
 
-//<---------------------------- Options -------------------------------------->
-//<<< Configuration switch >>>
+/**
+ * @def Configuration mode
+ */
 #define MR_CONF_DISABLE                 0
-#define MR_CONF_ENABLE                  1
+#define MR_CFG_ENABLE                   1
 
-//<<< Debug level >>>
-#define MR_CONF_DEBUG_ASSERT            0
-#define MR_CONF_DEBUG_ERROR             1
-#define MR_CONF_DEBUG_WARNING           2
-#define MR_CONF_DEBUG_INFO              3
-#define MR_CONF_DEBUG_DEBUG             4
+/**
+ * @def Debug level
+ */
+#define MR_CFG_DEBUG_LEVEL_ASSERT       0
+#define MR_CFG_DEBUG_LEVEL_ERROR        1
+#define MR_CFG_DEBUG_LEVEL_WARNING      2
+#define MR_CFG_DEBUG_LEVEL_INFO         3
+#define MR_CFG_DEBUG_LEVEL_DEBUG        4
 
 //<---------------------------- Configuration -------------------------------------->
-//<<< Object >>>
-#define MR_CONF_NAME_MAX                12
+/**
+ * @def Object name size
+ *
+ * Recommend size: 2^n - 1.
+ */
+#define MR_CFG_OBJECT_NAME_SIZE         15
 
-//<<< Debug >>>
-#define MR_CONF_DEBUG                   MR_CONF_ENABLE
-#if (MR_CONF_DEBUG == MR_CONF_ENABLE)
-#define MR_CONF_ASSERT                  MR_CONF_ENABLE
-#define MR_CONF_DEBUG_LEVEL             MR_CONF_DEBUG_DEBUG
+/**
+ * @def Auto init config
+ *
+ * MR_CFG_DISABLE: Disable auto init
+ * MR_CFG_ENABLE: Enable auto init
+ */
+#define MR_CFG_AUTO_INIT                MR_CFG_ENABLE
+
+/**
+ * @def FSM config
+ *
+ * MR_CFG_DISABLE: Disable fsm
+ * MR_CFG_ENABLE: Enable fsm
+ */
+#define MR_CFG_FSM                      MR_CFG_ENABLE
+
+/**
+ * @def Event config
+ *
+ * MR_CFG_DISABLE: Disable event
+ * MR_CFG_ENABLE: Enable event
+ */
+#define MR_CFG_EVENT                    MR_CFG_ENABLE
+
+/**
+ * @def Soft timer config
+ *
+ * MR_CFG_DISABLE: Disable soft timer
+ * MR_CFG_ENABLE: Enable soft timer
+ */
+#define MR_CFG_SOFT_TIMER               MR_CFG_ENABLE
+
+/**
+ * @def Debug config
+ *
+ * MR_CFG_DISABLE: Disable debug
+ * MR_CFG_ENABLE: Enable debug
+ */
+#define MR_CFG_DEBUG                    MR_CFG_ENABLE
+
+#if (MR_CFG_DEBUG == MR_CFG_ENABLE)
+
+/**
+ * @def Assert config
+ *
+ * MR_CFG_DISABLE: Disable assert
+ * MR_CFG_ENABLE: Enable assert
+ */
+#define MR_CFG_ASSERT                   MR_CFG_ENABLE
+
+/**
+ * @def Debug level
+ *
+ * MR_CFG_DEBUG_LEVEL_ASSERT: Assert
+ * MR_CFG_DEBUG_LEVEL_ERROR: Error
+ * MR_CFG_DEBUG_LEVEL_WARNING: Warning
+ * MR_CFG_DEBUG_LEVEL_INFO: Info
+ * MR_CFG_DEBUG_LEVEL_DEBUG: Debug
+ */
+#define MR_CFG_DEBUG_LEVEL              MR_CFG_DEBUG_LEVEL_DEBUG
+
+/**
+ * @def Debug console name
+ */
+#define MR_CFG_DEBUG_CONSOLE_NAME       "uart1"
+
 #endif
 
-//<<< Server >>>
-#define MR_CONF_SERVER                  MR_CONF_ENABLE
-#if (MR_CONF_SERVER == MR_CONF_ENABLE)
-//<< fsm >>
-#define MR_CONF_FSM                     MR_CONF_ENABLE
-//<< event >>
-#define MR_CONF_EVENT                   MR_CONF_ENABLE
-//<< soft-timer >>
-#define MR_CONF_SOFT_TIMER              MR_CONF_ENABLE
+/**
+ * @def Device config
+ *
+ * MR_CFG_DISABLE: Disable device
+ * MR_CFG_ENABLE: Enable device
+ */
+#define MR_CFG_DEVICE                   MR_CFG_ENABLE
+
+#if (MR_CFG_DEVICE == MR_CFG_ENABLE)
+
+/**
+ * @def Pin config
+ *
+ * MR_CFG_DISABLE: Disable pin
+ * MR_CFG_ENABLE: Enable pin
+ */
+#define MR_CFG_PIN                      MR_CFG_ENABLE
+
+/**
+ * @def Serial config
+ *
+ * MR_CFG_DISABLE: Disable serial
+ * MR_CFG_ENABLE: Enable serial
+ */
+#define MR_CFG_SERIAL                  MR_CFG_ENABLE
+
+/**
+ * @def ADC config
+ *
+ * MR_CFG_DISABLE: Disable adc
+ * MR_CFG_ENABLE: Enable adc
+ */
+#define MR_CFG_ADC                      MR_CFG_ENABLE
+
+/**
+ * @def DAC config
+ *
+ * MR_CFG_DISABLE: Disable dac
+ * MR_CFG_ENABLE: Enable dac
+ */
+#define MR_CFG_DAC                      MR_CFG_ENABLE
+
+/**
+ * @def I2C config
+ *
+ * MR_CFG_DISABLE: Disable i2c
+ * MR_CFG_ENABLE: Enable i2c
+ */
+#define MR_CFG_I2C                      MR_CFG_ENABLE
+
+/**
+ * @def SPI config
+ *
+ * MR_CFG_DISABLE: Disable spi
+ * MR_CFG_ENABLE: Enable spi
+ */
+#define MR_CFG_SPI                       MR_CFG_ENABLE
+
 #endif
 
-//<<< Device >>>
-#define MR_CONF_DEVICE                  MR_CONF_ENABLE
-#if (MR_CONF_DEVICE == MR_CONF_ENABLE)
-//<< pin >>
-#define MR_CONF_PIN                     MR_CONF_ENABLE
-//<< serial >>
-#define MR_CONF_SERIAL                  MR_CONF_ENABLE
-#if (MR_CONF_SERIAL == MR_CONF_ENABLE)
-#define MR_CONF_SERIAL_RX_BUFSZ         32
-#define MR_CONF_SERIAL_TX_BUFSZ         0
-#define MR_CONF_SERIAL_RX_DMA_BUFS      0
-#define MR_CONF_SERIAL_TX_DMA_BUFS      0
-//< console >
-#define MR_CONF_CONSOLE                 MR_CONF_DISABLE
-#define MR_CONF_CONSOLE_NAME            "uart1"
-#endif
-//<< spi >>
-#define MR_CONF_SPI                     MR_CONF_ENABLE
-#if (MR_CONF_SPI == MR_CONF_ENABLE)
-#define MR_CONF_SPI_RX_BUFSZ            32
-#define MR_CONF_SPI_TX_BUFSZ            4
-#define MR_CONF_SPI_RX_DMA_BUFS         0
-#define MR_CONF_SPI_TX_DMA_BUFS         0
-#endif
-//<< i2c >>
-#define MR_CONF_I2C                     MR_CONF_ENABLE
-//<< adc >>
-#define MR_CONF_ADC                     MR_CONF_ENABLE
-//<< dac >>
-#define MR_CONF_DAC                     MR_CONF_ENABLE
 //<< timer >>
-#define MR_CONF_TIMER                   MR_CONF_ENABLE
+#define MR_CONF_TIMER                   MR_CFG_ENABLE
 //<< pwm >>
-#define MR_CONF_PWM                     MR_CONF_ENABLE
-#endif
+#define MR_CONF_PWM                     MR_CFG_ENABLE
 
 #endif /* _MR_CONFIG_H_ */
