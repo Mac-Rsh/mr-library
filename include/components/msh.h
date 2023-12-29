5178b61d9607f6ecb73fde116b9c4e5c5ec03c76
MacRsh <MacRsh@outlook.com>
2023-12-30 03:51:50 +0800

    1.新增msh组件（类Shell工具）。

---
+++ include/components/msh.h
@@ -0,0 +1,77 @@
+/*
+ * @copyright (c) 2023, MR Development Team
+ *
+ * @license SPDX-License-Identifier: Apache-2.0
+ *
+ * @date 2023-12-25    MacRsh       First version
+ */
+
+#ifndef _MSH_H_
+#define _MSH_H_
+
+#include "include/mr_api.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef MR_USING_MSH
+
+/**
+ * @brief MSH command structure.
+ */
+struct mr_msh_cmd
+{
+    const char *name;                                               /**< Name */
+    int (*call)(int argc, void *args);                              /**< Callback function */
+    const char *help;                                               /**< Help information */
+};
+
+/**
+ * @brief Exports a MSH command with level.
+ *
+ * @param name The name of the command.
+ * @param fn The callback function.
+ * @param help The help information.
+ * @param level The level of the command.
+ */
+#define MR_MSH_EXPORT(name, fn, help, level) \
+    MR_USED const struct mr_msh_cmd _mr_msh_cmd_##name MR_SECTION(".msh_cmd."level) = {#name, fn, help};
+
+/**
+ * @brief Exports a MSH command.
+ *
+ * @param name The name of the command.
+ * @param fn The callback function.
+ * @param help The help information.
+ */
+#define MR_MSH_CMD_EXPORT(name, fn, help) \
+    MR_MSH_EXPORT(name, fn, help, "1")
+
+/**
+ * @brief Parses the arguments for the MSH command.
+ *
+ * @param index The index of the argument.
+ *
+ * @note This macro must be called from a function where the first parameter is argc and the second parameter is args.
+ *       1 -> argc, 2 -> args.
+ */
+#define MR_MSH_PARSE_ARGS(index) \
+    (((index) < (argc)) ? (((const char **)(args))[index]) : MR_NULL)
+
+/**
+ * @addtogroup Msh.
+ * @{
+ */
+void mr_msh_recv_char(char c);
+/** @} */
+#else
+#define MR_MSH_EXPORT(name, fn, help, level)
+#define MR_MSH_CMD_EXPORT(name, fn, help)
+#endif /* MR_USING_MSH */
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _MSH_H_ */
