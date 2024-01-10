b1d589ac126524ece29988f575bd8573c1a0dd25
MacRsh <MacRsh@outlook.com>
2024-01-10 17:13:11 +0800

    1.文件优化。

--- include/mr_lib.h
+++ include/mr_lib.h
@@ -1,31 +1,26 @@
-/*
- * @copyright (c) 2023, MR Development Team
- *
- * @license SPDX-License-Identifier: Apache-2.0
- *
- * @date 2023-10-20    MacRsh       First version
- */
-
 #ifndef _MR_LIB_H_
 #define _MR_LIB_H_
-
-#include "mr_api.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
-#include "device/adc.h"
-#include "device/can.h"
-#include "device/dac.h"
-#include "device/i2c.h"
-#include "device/soft_i2c.h"
-#include "device/pin.h"
-#include "device/serial.h"
-#include "device/spi.h"
-#include "device/timer.h"
-
-#include "components/msh.h"
+#include "mr_api.h"
+#include "mr_config.h"
+#include "mr_def.h"
+#include "mr_lib.h"
+#include "mr_service.h"
+#include "components/mr_msh.h"
+#include "device/mr_adc.h"
+#include "device/mr_can.h"
+#include "device/mr_dac.h"
+#include "device/mr_i2c.h"
+#include "device/mr_pin.h"
+#include "device/mr_pwm.h"
+#include "device/mr_serial.h"
+#include "device/mr_soft_i2c.h"
+#include "device/mr_spi.h"
+#include "device/mr_timer.h"
 
 #ifdef __cplusplus
 }
